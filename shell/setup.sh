5e673fe9ae432b49cb9f1c7c990ad0ebfd126096
Morteza Shahriari Nia <info@mshahriarinia.com>
2013-10-23 16:35:48 -0400

    Avoid replacing .bashrc to mac as there are inconsistencies sometimes

--- shell/setup.sh
+++ shell/setup.sh
@@ -6,16 +6,29 @@ rm -rf  ~/cheatsheetBackup
 mkdir ~/cheatsheetBackup
 
 echo "Backing up current settings to directory ~/cheatsheetBackup.."
+
+
+if [ "$(uname)" == "Darwin" ]; then
+    # Do something under Mac OS X platform        
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    # Do something under Linux platform
+    mv ~/.bashrc ~/cheatsheetBackup/.bashrc
+    cp .bashrc ~/
+elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
+    # Do something under Windows NT platform
+fi
+
 mv ~/.vimrc ~/cheatsheetBackup/.vimrc.bak
 mv ~/.vim ~/cheatsheetBackup/.vim.bak
-mv ~/.bashrc ~/cheatsheetBackup/.bashrc
 mv ~/.tmux ~/cheatsheetBackup/.tmux
 mv ~/.tmux.conf ~/cheatsheetBackup/.tmux.conf
 
 echo "Moving new settings in place.."
 cp .vimrc ~/
 cp -R .vim/ ~/
-cp .bashrc ~/
+
+
+
 cp .tmux.conf ~/
 cp -R .tmux/ ~/
 
