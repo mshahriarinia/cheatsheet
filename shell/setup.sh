ac25132fee27e4386332641f0eb7128eead2c2c5
Morteza Shahriari Nia <info@mshahriarinia.com>
2013-10-23 16:26:07 -0400

    Add AutoComplPop to vim plugins.

--- shell/setup.sh
+++ shell/setup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 
+rm -rf  ~/cheatsheetBackup
+
 mkdir ~/cheatsheetBackup
 
 echo "Backing up current settings to directory ~/cheatsheetBackup.."
@@ -10,4 +12,32 @@ mv ~/.bashrc ~/cheatsheetBackup/.bashrc
 mv ~/.tmux ~/cheatsheetBackup/.tmux
 mv ~/.tmux.conf ~/cheatsheetBackup/.tmux.conf
 
-mv * ~/
+echo "Moving new settings in place.."
+cp .vimrc ~/
+cp -R .vim/ ~/
+cp .bashrc ~/
+cp .tmux.conf ~/
+cp -R .tmux/ ~/
+
+echo "Downloading plugins..."
+
+wget https://bitbucket.org/ns9tks/vim-autocomplpop/get/tip.zip
+unzip tip.zip
+rm tip.zip
+cd n*
+cp -R * ~/.vim
+cd ..
+rm -rf n*
+
+wget   https://bitbucket.org/ns9tks/vim-l9/get/tip.zip 
+unzip tip.zip
+rm tip.zip
+cd n*
+cp -R * ~/.vim
+cd ..
+rm -rf n*
+
+
+
+
+
